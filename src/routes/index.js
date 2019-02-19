00b70ebdb30546d6a3373c39d3a045b248615965
David Banham <david@banham.id.au>
2019-02-19 13:23:03 +1100

    Clean up routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,14 @@
 const { Router } = require('express');
 const express = require('express');
-const users = require('./users');
-const groups = require('./groups');
-const sites = require('./sites');
-const checks = require('./checks');
-const nominees = require('./nominees');
-const markers = require('./markers');
-const things = require('./things');
 
 const v1 = new Router()
-  .use('/users', users)
-  .use('/groups', groups)
-  .use('/sites', sites)
-  .use('/checks', checks)
-  .use('/nominees', nominees)
-  .use('/markers', markers)
-  .use('/things', things);
+  .use('/users', require('./users'))
+  .use('/groups', require('./groups'))
+  .use('/sites', require('./sites'))
+  .use('/checks', require('./checks'))
+  .use('/nominees', require('./nominees'))
+  .use('/markers', require('./markers'))
+  .use('/things', require('./things'));
 
 const router = module.exports = new Router();
 
